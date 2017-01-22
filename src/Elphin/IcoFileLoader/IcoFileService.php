5c81f33632a8920c1f84d3a049ac020634dfd342
Paul Dixon <paul@liblynx.com>
2017-01-22 16:46:59 +0000

    improved phpdoc comment readability

--- src/Elphin/IcoFileLoader/IcoFileService.php
+++ src/Elphin/IcoFileLoader/IcoFileService.php
@@ -22,10 +22,10 @@ class IcoFileService
      * IcoFileService constructor
      *
      * You can inject alternative implementations of the renderer or parser, but for most
-     * typical uses, you can accept the defaults
+     * typical uses, you can accept the defaults.
      *
      * @param RendererInterface|null $renderer
-     * @param ParserInterface|null $parser
+     * @param ParserInterface|null   $parser
      */
     public function __construct(RendererInterface $renderer = null, ParserInterface $parser = null)
     {
@@ -42,18 +42,18 @@ class IcoFileService
      * This will either return a valid image, or will throw an \InvalidArgumentException in the event of the file
      * being unreadable.
      *
-     * @param string $dataOrFile either a filename to a .ico file, or binary data from an .ico file in a string
-     * @param integer $w desired width. The class tries to locate the best quality image at this size, but
-     *                            if not found, the largest available icon will be used and resized to fit
-     * @param integer $h desired height - as icons are usually square, this should be same as $w
-     * @param array $opts array of renderer options. The built in renderer supports an optional 'background'
+     * @param string  $dataOrFile Either a filename to a .ico file, or binary data from an .ico file in a string
+     * @param integer $w          Desired width. The class tries to locate the best quality image at this size, but
+     *                            if not found, the largest available icon will be used and resized to fit.
+     * @param integer $h          Desired height - as icons are usually square, this should be same as $w.
+     * @param array   $opts       Array of renderer options. The built in renderer supports an optional 'background'
      *                            elemen in this array. Normally, the result will use alpha transparency, but you can
      *                            pass a hex colour to choose the colour of the transparent area instead, e.g.
-     *                            ['background=>'#ffffff'] for a white background
-     * @return mixed              the built in renderer will return a gd image resource, which you could save with
+     *                            ['background=>'#ffffff'] for a white background.
+     * @return mixed              The built in renderer will return a gd image resource, which you could save with
      *                            the gd function imagepng(), for example. If you swap in an alternative renderer,
      *                            the result is whatever that renderer returns.
-     * @throws \InvalidArgumentException if file is not found or is invalid
+     * @throws \InvalidArgumentException if file is not found or is invalid.
      */
     public function extractIcon($dataOrFile, $w, $h, array $opts = null)
     {
@@ -67,19 +67,19 @@ class IcoFileService
     }
 
     /**
-     * Renders an IconImage at a desired width and height
+     * Renders an IconImage at a desired width and height.
      *
-     * @param IconImage $image image obtained from an Icon object
-     * @param integer $w desired width - if null, width of IconImage is used
-     * @param integer $h desired height - if null, height of IconImage is used
-     * @param array $opts array of renderer options. The built in renderer supports an optional 'background'
-     *                            elemen in this array. Normally, the result will use alpha transparency, but you can
+     * @param IconImage $image    Image obtained from an Icon object.
+     * @param integer   $w        Desired width - if null, width of IconImage is used.
+     * @param integer   $h        Desired height - if null, height of IconImage is used.
+     * @param array     $opts     Array of renderer options. The built in renderer supports an optional 'background'
+     *                            element in this array. Normally, the result will use alpha transparency, but you can
      *                            pass a hex colour to choose the colour of the transparent area instead, e.g.
-     *                            ['background=>'#ffffff'] for a white background
-     * @return mixed              the built in renderer will return a gd image resource, which you could save with
+     *                            ['background=>'#ffffff'] for a white background.
+     * @return mixed              The built in renderer will return a gd image resource, which you could save with
      *                            the gd function imagepng(), for example. If you swap in an alternative renderer,
      *                            the result is whatever that renderer returns.
-     * @throws \InvalidArgumentException if IconImage or options are invalid
+     * @throws \InvalidArgumentException if IconImage or options are invalid.
      */
     public function renderImage(IconImage $image, $w = null, $h = null, array $opts = null)
     {
@@ -90,12 +90,12 @@ class IcoFileService
     }
 
     /**
-     * Parses a .ico file from a pathname or binary data string and return an Icon object
+     * Parses a .ico file from a pathname or binary data string and return an Icon object.
      *
      * This is a useful lower level member which can be used to inspect an icon before
-     * rendering a particular image within it with renderImage
+     * rendering a particular image within it with renderImage.
      *
-     * @param string $dataOrFile either filename or binary data
+     * @param string $dataOrFile Either filename or binary data.
      * @return Icon
      * @throws \InvalidArgumentException if file is not found or invalid
      */
@@ -108,7 +108,8 @@ class IcoFileService
     }
 
     /**
-     * Loads icon from file
+     * Loads icon from file.
+     *
      * @param string $file filename or URL (if fopen wrappers installed)
      * @return Icon
      * @throws \InvalidArgumentException if file is not found or invalid
@@ -123,7 +124,8 @@ class IcoFileService
     }
 
     /**
-     * Loads icon from string
+     * Loads icon from string.
+     *
      * @param string $data binary data string containing a .ico file
      * @return Icon
      * @throws \InvalidArgumentException if file is not found or invalid
